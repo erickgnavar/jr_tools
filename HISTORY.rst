e0186376b339c47b8dca94850ec05134f8d21b86
Erick Navarro <erick@navarro.io>
2017-10-31 08:12:50 -0500

    Bump version

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,11 @@
 History
 =======
 
+0.2.0 (2017-10-31)
+------------------
+
+* Add suport to upload and configure files and reports to JasperServer using a yml file
+
 0.1.0 (2017-07-30)
 ------------------
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = []
 
 setup(
     name='jr_tools',
-    version='0.1.1',
+    version='0.2.0',
     description="A collection of tools to handle Jasper Reports with python",
     long_description=readme + '\n\n' + history,
     author="Erick Navarro",
